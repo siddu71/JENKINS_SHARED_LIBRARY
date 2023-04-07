ec02fc4b08b99f84d94f2f4a283d4f89e1c2889d
siddu71 <52039971+siddu71@users.noreply.github.com>
2023-04-07 16:42:12 +0100

    docker build

---
+++ vars/DockerBuild.groovy
@@ -0,0 +1,7 @@
+def call(String project, String hubUser, String ImageTag){
+  sh """
+  docker image build -t $(hubUser):$(project) .
+  docker image tag $(hubUser):$(project) $(hubUser)/$(project):$(ImageTag)
+  docker image tag $(hubUser):$(project) $(hubUser)/$(project):latest
+  """
+}
