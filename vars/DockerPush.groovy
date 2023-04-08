b296b5991da25b1152e16d2c62267110325b9c6c
siddu71 <52039971+siddu71@users.noreply.github.com>
2023-04-08 03:24:39 +0100

    docker image Push

--- vars/DockerPush.groovy
+++ vars/DockerPush.groovy
@@ -2,7 +2,7 @@ def call(String project, String ImageTag, String hubUser){
     withCredentials([usernamePassword(credentialsId: 'docker', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
     sh "docker login -u '$USER' -p '$PASS'"
     }
-    docker push ${hubUser}/${project}:${ImageTag}
-    docker push ${hubUser}/${project}:v1
+    docker image push ${hubUser}/${project}:${ImageTag}
+    docker image push ${hubUser}/${project}:v1
   
 }
