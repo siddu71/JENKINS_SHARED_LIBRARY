50efe3f8afb631eb10dd187c2465572fd6abe957
siddu71 <52039971+siddu71@users.noreply.github.com>
2023-04-16 20:08:33 +0100

    docker image Push

--- vars/DockerPushECR.groovy
+++ vars/DockerPushECR.groovy
@@ -1,7 +1,8 @@
 def call(String aws_account_id, String region, String ecr_reponame){
     
     sh """
-    aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${aws_account_id}.dkr.ecr.${region}.amazonaws.com
+    aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 834920522358.dkr.ecr.us-east-1.amazonaws.com
+
     docker push ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_reponame}:latest
     """
 
