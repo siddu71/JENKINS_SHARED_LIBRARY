238f1ee3a4e401eecd67ecd24c0c38f5369faebb
siddu71 <52039971+siddu71@users.noreply.github.com>
2023-04-16 19:22:50 +0100

    docker image Push

--- vars/DockerImageCleanup.groovy
+++ vars/DockerImageCleanup.groovy
@@ -1,4 +1,5 @@
 def call(String project, String ImageTag, String hubUser){
+  
     sh """
      docker rmi ${hubUser}/${project}:${ImageTag}
      docker rmi ${hubUser}/${project}:latest
---
+++ vars/DockerImageScanECR.groovy
@@ -0,0 +1,7 @@
+def call(String aws_account_id, String region, String ecr_reponame){
+    
+    sh """
+     trivy scan ${ecr_reponame}:latest ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_reponame}:latest > scan.txt
+     cat scan.txt
+    """
+}
